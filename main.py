3051afd8a928c97964c5beb4ae9d2eb79b73af9e
dairuinz <45816589+dairuinz@users.noreply.github.com>
2022-02-20 17:33:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ import calendar
 warnings.filterwarnings('ignore')
 
 def main():
-    df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding='latin-1')
-    df.columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
+    df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding='latin-1')       #reads csv
+    df.columns = ['target', 'ids', 'date', 'flag', 'user', 'text']          #names the columns
 
-    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_columns', None)        #shows dataframe with no column limitat terminal
 
     work_df = df[799500:800500]     #reducing the sample size for speed (specific choice so we have 0 and 4 as target)
 
@@ -98,17 +98,17 @@ def wordtovec(tweets):
     # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     # import gensim.downloader as api
     # corpus = api.load('text8')
-    # import inspect
+    # import inspect                                                                                    #downloads ready vocabulary, comment if alreadt saved
     # print(inspect.getsource(corpus.__class__))
     # print(inspect.getfile(corpus.__class__))
     # model = Word2Vec(corpus)
     # model.save('./readyvocab.model')
 
-    model = Word2Vec.load('readyvocab.model')
+    model = Word2Vec.load('readyvocab.model')       #reads the vocabulary
 
     processed_sentences = []
     for sentence in tweets:
-        processed_sentences.append(gensim.utils.simple_preprocess(sentence))
+        processed_sentences.append(gensim.utils.simple_preprocess(sentence))        #for every sentence in tweets tokenizes each words
 
     vectors = {}
     i = 0
@@ -116,22 +116,22 @@ def wordtovec(tweets):
         vectors[str(i)] = []
         for k in v:
             try:
-                vectors[str(i)].append(model.wv[k].mean())
+                vectors[str(i)].append(model.wv[k].mean())      #appends the vector of the word
             except:
-                vectors[str(i)].append(np.nan)
+                vectors[str(i)].append(np.nan)      #if the word doesnt exist the vocabulary insert it as a Nan value
         i += 1
 
-    df_input = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in vectors.items()]))
-    df_input.fillna(value=0.0, inplace=True)  # oti einai nan paei 0
+    df_input = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in vectors.items()]))      #puts the vectors in a dataframe
+    df_input.fillna(value=0.0, inplace=True)        #replace Nan values with 0
 
-    df_input = df_input.transpose()
+    df_input = df_input.transpose()     #transposes the matrices in order to insert into the models
 
     return df_input
 
 def date_prep(work_df):
     work_df['date_year'] = work_df['date'].dt.year
     work_df['date_month'] = work_df['date'].dt.month
-    work_df['date_day'] = work_df['date'].dt.day
+    work_df['date_day'] = work_df['date'].dt.day            #makes each date information into a new collumn, year, month, etc
     work_df['date_hour'] = work_df['date'].dt.hour
     work_df['date_min'] = work_df['date'].dt.minute
     work_df['date_sec'] = work_df['date'].dt.second
@@ -142,7 +142,7 @@ def vectorizer(tweets):
     bow_vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
     bow = bow_vectorizer.fit_transform(tweets['clean'])      #creates a  sparse matrix (mostly zeros) with words
 
-    vec = pd.DataFrame.sparse.from_spmatrix(bow)
+    vec = pd.DataFrame.sparse.from_spmatrix(bow)        #puts the vectors in a database
     # print(vec)
 
     return vec
@@ -166,30 +166,30 @@ def model_training(bow, target):
         n_iter=50       #how many times it checks them
         )
 
-    random_search_cv.fit(X_train, y_train)
+    random_search_cv.fit(X_train, y_train)      #fits the data into the model
     print(f'\nBest parameters for Logistic Regression: {random_search_cv.best_params_}')
 
-    log = random_search_cv.best_estimator_
+    log = random_search_cv.best_estimator_      #logistic regression with the best parameters
 
-    pred = log.predict(X_test)
-    print('\nf1_score: ', f1_score(y_test, pred, pos_label=0))
-    print('log_score: ', log.score(X_test, y_test))
-    print('accuracy_score: ', accuracy_score(y_test, pred))
+    pred = log.predict(X_test)      #prediction of logistic regression
+    print('\nf1_score: ', f1_score(y_test, pred, pos_label=0))      #f1 score
+    print('log_score: ', log.score(X_test, y_test))     #logistic regression score
+    print('accuracy_score: ', accuracy_score(y_test, pred))     #accuracy_score
     # print('Probability to be: \n   Positive / Negative: \n', log.predict_proba(X_test))
 
     print('\nScores after probability rework: ')
     pred_proba = log.predict_proba(X_test)      #we try to increase score
     pred2 = pred_proba[:, 0] >= 0.35      #>35% positive counts as positive instead of >50%
-    pred2 = pred.astype(np.int)
-    print('f1_score: ', f1_score(y_test, pred2, average='micro', pos_label=0))
-    print('log_score: ', log.score(X_test, y_test))
-    print('accuracy_score: ', accuracy_score(y_test, pred2))
+    pred2 = pred.astype(np.int)     #transforms the result to an integer
+    print('f1_score: ', f1_score(y_test, pred2, average='micro', pos_label=0))      #f1 score
+    print('log_score: ', log.score(X_test, y_test))     #logistic regression score
+    print('accuracy_score: ', accuracy_score(y_test, pred2))        #accuracy_score
 
     #Decision Tree Classifier
     print('\nDecision Tree Classifier:')
     scores = cross_val_score(DecisionTreeClassifier(), X_train, y_train, cv=5)  #5 different parameters test
     # print(scores)
-    print('\nMean DTC score: ', scores.mean(), sep='')
+    print('\nMean DTC score: ', scores.mean(), sep='')      #prints mean score of the 5 different parameters
 
     bag_model = BaggingClassifier(base_estimator=DecisionTreeClassifier(),       #100 models with different samples and takes the majority vote for score
                                   n_estimators=100,
@@ -198,12 +198,13 @@ def model_training(bow, target):
                                   random_state=0)
 
     bag_model.fit(X_train, y_train)
-    print('Bagging score: ', bag_model.score(X_test, y_test), sep='')
+    print('Bagging score: ', bag_model.score(X_test, y_test), sep='')       #bagging score
 
-    print('Mean Bagging score: ', cross_val_score(bag_model, X_train, y_train, cv=5).mean(), sep='')
+    print('Mean Bagging score: ', cross_val_score(bag_model, X_train, y_train, cv=5).mean(), sep='')        #cross_val_score
 
+    print('\nRandom Forest Classifier:')
     from sklearn.ensemble import RandomForestClassifier
-    print('Mean Random Forest score: ', cross_val_score(RandomForestClassifier(), X_train, y_train, cv=5).mean(), sep='')
+    print('Mean Random Forest score: ', cross_val_score(RandomForestClassifier(), X_train, y_train, cv=5).mean(), sep='')       #cross_val_score of Random Forest Classifier
 
 def hashtag_frequency(tweets_df):
     ht_positive = hashtag_extract(tweets_df['clean'][tweets_df['target']==4])       #thetika hashtags
@@ -215,16 +216,16 @@ def hashtag_frequency(tweets_df):
     if q == 'y':
         a = input('Press ~p~ for positive or ~n~ for negative: ')
         if a == 'p':
-            freq = nltk.FreqDist(ht_positive)
-            d = pd.DataFrame({'Hashtag': list(freq.keys()),
-                              'Count': list(freq.values())})
+            freq = nltk.FreqDist(ht_positive)       #puts the positive hashtags into ntlk to find frequency
+            d = pd.DataFrame({'Hashtag': list(freq.keys()),     #prints hashtag
+                              'Count': list(freq.values())})        #prints frequency of hashtag
             h = input('How many hashtags to show? Type a number: ')
-            d = d.nlargest(columns='Count', n=int(h))
-            plt.figure(figsize=(15,9))
-            sns.barplot(data=d, x='Hashtag', y='Count')
+            d = d.nlargest(columns='Count', n=int(h))       #number of hashtags you want to print
+            plt.figure(figsize=(15,9))      #size of graph
+            sns.barplot(data=d, x='Hashtag', y='Count')     #puts data into the graph, names x, y axis
             plt.show()
         elif a == 'n':
-            freq = nltk.FreqDist(ht_negative)
+            freq = nltk.FreqDist(ht_negative)       #same as above but for negative
             d = pd.DataFrame({'Hashtag': list(freq.keys()),
                               'Count': list(freq.values())})
             h = input('How many hashtags to show? Type a number: ')
@@ -238,7 +239,7 @@ def hashtag_frequency(tweets_df):
 def hashtag_extract(tweets):
     hashtags = []
     for tweet in tweets:
-        ht = re.findall(r'#(\w+)', tweet)   #finds hashtags
+        ht = re.findall(r'#(\w+)', tweet)   #finds hashtags in each tweet
         hashtags.append(ht)     #appends them
 
     return hashtags
@@ -248,15 +249,15 @@ def visualization(tweets_df):
     if q == 'y':
         a = input('Press ~p~ for positive or ~n~ for negative: ')
         if a == 'p':
-            all_words = ' '.join([sentence for sentence in tweets_df['clean'][tweets_df['target']==4]])     #puts all words together
+            all_words = ' '.join([sentence for sentence in tweets_df['clean'][tweets_df['target']==4]])     #puts all words together for positive
             wordcloud = WordCloud(width=800, height=500, random_state=42, max_font_size=100).generate(all_words)        #loads them to wordcloud
 
-            plt.figure(figsize=(15, 8))
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis('off')
+            plt.figure(figsize=(15, 8))     #size of graph
+            plt.imshow(wordcloud, interpolation='bilinear')     #puts wordcloud into image
+            plt.axis('off')     #no axis
             plt.show()
         elif a == 'n':
-            all_words = ' '.join([sentence for sentence in tweets_df['clean'][tweets_df['target']==0]])
+            all_words = ' '.join([sentence for sentence in tweets_df['clean'][tweets_df['target']==0]])     #the save as above but for negative
             wordcloud = WordCloud(width=800, height=500, random_state=42, max_font_size=100).generate(all_words)
 
             plt.figure(figsize=(15, 8))
@@ -268,11 +269,11 @@ def visualization(tweets_df):
 
 def data_preprocessing(tweets_df):
 
-    date_trash = ['PDT', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    months = {'Jan': '1', 'Feb': '2', 'Apr': '3', 'Mar': '4', 'May': '5', 'Jun': '6', 'Jul': '7', 'Aug': '8', 'Sep': '9', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+    date_trash = ['PDT', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']       #words we want to remove
+    months = {'Jan': '1', 'Feb': '2', 'Apr': '3', 'Mar': '4', 'May': '5', 'Jun': '6', 'Jul': '7', 'Aug': '8', 'Sep': '9', 'Oct': '10', 'Nov': '11', 'Dec': '12'}        #dictionary with months and their number in calendar
 
     for x in date_trash:
-        tweets_df['date'] = tweets_df['date'].str.replace(x, '')      #deletes name of days and PDT
+        tweets_df['date'] = tweets_df['date'].str.replace(x, '')      #deletes name of days and 'PDT'
 
     for k, v in months.items():
         tweets_df.date = tweets_df.date.str.replace(k, v)       #converts months to numbers with the help of dictionary created above
@@ -296,7 +297,7 @@ def data_preprocessing(tweets_df):
     for i in range(len(token_tweets)):
         token_tweets.iloc[i] = ' '.join(token_tweets.iloc[i])
 
-    tweets_df['clean'] = token_tweets
+    tweets_df['clean'] = token_tweets       #creates new column with the processed tweets
     # print(tweets_df[:10])
 
     return tweets_df
@@ -304,7 +305,7 @@ def data_preprocessing(tweets_df):
 def remove_pattern(tweets, pattern):    #deletes what we insert
     r = re.findall(pattern, tweets)
     for word in r:
-        tweets = re.sub(word, '', tweets)
+        tweets = re.sub(word, '', tweets)       #subtracts the given word
     return tweets
 
 
